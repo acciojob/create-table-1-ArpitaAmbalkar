657e942f02914f001d890d1303bd3516d0aea4b2
ArpitaAmbalkar <arpitaambalkar26@gmail.com>
2025-10-05 20:25:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
     //Write your code here
-  
+	let table=document.getElementById("sampleTable");
+  let tr=document.createElement("tr");
+   let td1=document.createElement("td");
+	td1.textContent="Row2 cell1";
+	let td2=document.createElement("td");
+	td2.textContent="Row2 cell2";
+	tr.appendChild(td1);
+	tr.appendChild(td2);
+	table.appendChild(tr);
   
 }
